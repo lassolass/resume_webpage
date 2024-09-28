af1bbd39147ce664b5eb2d82b7285edfa6ec0c5a
lassolass <kobanilass@gmail.com>
2024-09-28 23:50:48 +0100

    Added JS file and buttons to hide and show the details of each job or education description. I also added a summary section so that while the details of each job or education are hidden, some key information is still always visible.

---
+++ scripts.js
@@ -0,0 +1,107 @@
+function togglebpReservoirEngineerParagraph() {
+    // get the section that is to be hidden unless the button is pressed
+    var blockParagraph = document.getElementById("bpReservoirEngineer");
+    var blockParagraphButton = document.getElementById("bpReservoirEngineerButton")
+    console.log(blockParagraphButton)
+    // get the data of whether the paragraph is visible or hidden
+    var blockParagraphDisplaySetting = blockParagraph.style.display;
+
+    if (blockParagraphDisplaySetting == "block") {
+        blockParagraph.style.display = "none";
+        blockParagraphButton.innerHTML = "▽"
+    }
+    else {
+        blockParagraph.style.display = "block";
+        blockParagraphButton.innerHTML = "△"
+    }
+}
+
+function toggleUoADemonstratorParagraph() {
+    // get the section that is to be hidden unless the button is pressed
+    var blockParagraph = document.getElementById("UoADemonstrator");
+    var blockParagraphButton = document.getElementById("UoADemonstratorButton")
+    console.log(blockParagraphButton)
+    // get the data of whether the paragraph is visible or hidden
+    var blockParagraphDisplaySetting = blockParagraph.style.display;
+
+    if (blockParagraphDisplaySetting == "block") {
+        blockParagraph.style.display = "none";
+        blockParagraphButton.innerHTML = "▽"
+    }
+    else {
+        blockParagraph.style.display = "block";
+        blockParagraphButton.innerHTML = "△"
+    }
+}
+
+function toggleConocoPhillipsInternParagraph() {
+    // get the section that is to be hidden unless the button is pressed
+    var blockParagraph = document.getElementById("ConocoPhillipsIntern");
+    var blockParagraphButton = document.getElementById("ConocoPhillipsInternButton")
+    console.log(blockParagraphButton)
+    // get the data of whether the paragraph is visible or hidden
+    var blockParagraphDisplaySetting = blockParagraph.style.display;
+
+    if (blockParagraphDisplaySetting == "block") {
+        blockParagraph.style.display = "none";
+        blockParagraphButton.innerHTML = "▽"
+    }
+    else {
+        blockParagraph.style.display = "block";
+        blockParagraphButton.innerHTML = "△"
+    }
+}
+
+function toggleCrescentPetroleumDanaGasTraineeParagraph() {
+    // get the section that is to be hidden unless the button is pressed
+    var blockParagraph = document.getElementById("CrescentPetroleumDanaGasTrainee");
+    var blockParagraphButton = document.getElementById("CrescentPetroleumDanaGasTraineeButton");
+    console.log(blockParagraphButton);
+    // get the data of whether the paragraph is visible or hidden
+    var blockParagraphDisplaySetting = blockParagraph.style.display;
+
+    if (blockParagraphDisplaySetting == "block") {
+        blockParagraph.style.display = "none";
+        blockParagraphButton.innerHTML = "▽";
+    }
+    else {
+        blockParagraph.style.display = "block";
+        blockParagraphButton.innerHTML = "△";
+    }
+}
+
+function toggleUoABachelorsParagraph() {
+    // get the section that is to be hidden unless the button is pressed
+    var blockParagraph = document.getElementById("UoABachelors");
+    var blockParagraphButton = document.getElementById("UoABachelorsButton")
+    console.log(blockParagraphButton)
+    // get the data of whether the paragraph is visible or hidden
+    var blockParagraphDisplaySetting = blockParagraph.style.display;
+
+    if (blockParagraphDisplaySetting == "block") {
+        blockParagraph.style.display = "none";
+        blockParagraphButton.innerHTML = "▽"
+    }
+    else {
+        blockParagraph.style.display = "block";
+        blockParagraphButton.innerHTML = "△"
+    }
+}
+
+function toggleCSMHighSchoolParagraph() {
+    // get the section that is to be hidden unless the button is pressed
+    var blockParagraph = document.getElementById("CSMHighSchool");
+    var blockParagraphButton = document.getElementById("CSMHighSchoolButton")
+    console.log(blockParagraphButton)
+    // get the data of whether the paragraph is visible or hidden
+    var blockParagraphDisplaySetting = blockParagraph.style.display;
+
+    if (blockParagraphDisplaySetting == "block") {
+        blockParagraph.style.display = "none";
+        blockParagraphButton.innerHTML = "▽"
+    }
+    else {
+        blockParagraph.style.display = "block";
+        blockParagraphButton.innerHTML = "△"
+    }
+}
